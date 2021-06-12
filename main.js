8088d54091c61a858de438ec4b92b007f6ca5d2e
RameenH26 <76857653+RameenH26@users.noreply.github.com>
2021-06-12 21:47:51 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@
 
  console.log('ml5 version', ml5.version);
 
- classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json', modelLoaded);
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ClMv2pj7p/model.json', modelLoaded);
 
 
  function modelLoaded() {
